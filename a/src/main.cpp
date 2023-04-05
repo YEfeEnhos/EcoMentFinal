d7b4b41a1e122dbdc119bfae64fe61d01b54c478
YEfeEnhos <enhyig.25@robcol.k12.tr>
2023-04-05 14:56:06 +0300

    a

--- a/src/main.cpp
+++ a/src/main.cpp
@@ -1,148 +1,369 @@
+/*    Max6675 Module  
+ *    CS              ==>     D10
+ *    SO              ==>     D12
+ *    SCK             ==>     D13
+ *    Vcc             ==>     Vcc (5v)
+ *    Gnd             ==>     Gnd      */
 #include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <PIDController.h>
-#include "max6675.h"
-// Define Rotary Encoder Pins
-#define CLK_PIN 21
-#define DATA_PIN 22
-#define SW_PIN 20
-// MAX6675 Pins
-#define thermoDO  26
-#define thermoCS  27
-#define thermoCLK  10
-// Mosfet Pin
-#define mosfet_pin 29
-// Serial Enable
-#define __DEBUG__
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-/*In this section we have defined the gain values for the 
- * proportional, integral, and derivative controller I have set
- * the gain values with the help of trial and error methods.
-*/ 
-#define __Kp 30 // Proportional constant
-#define __Ki 0.7 // Integral Constant
-#define __Kd 200 // Derivative Constant
-int clockPin; // Placeholder por pin status used by the rotary encoder
-int clockPinState; // Placeholder por pin status used by the rotary encoder
-int set_temperature = 1; // This set_temperature value will increas or decreas if when the rotarty encoder is turned
-float temperature_value_c = 0.0; // stores temperature value
-long debounce = 0; // Debounce delay
-int encoder_btn_count = 0; // used to check encoder button press
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO); // Create an instance for the MAX6675 Sensor Called "thermocouple"
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);// Create an instance for the SSD1306 128X64 OLED "display"
-PIDController pid; // Create an instance of the PID controller class, called "pid"
+
+//LCD config
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3f,20,4);  //sometimes the adress is not 0x3f. Change to 0x27 if it dosn't work.
+
+/*    i2c LCD Module  
+ *    SCL             ==>     A5
+ *    SDA             ==>     A4
+ *    Vcc             ==>     Vcc (5v)
+ *    Gnd             ==>     Gnd      */
+
+//I/O
+int PWM_pin = 3;  //Pin for PWM signal to the MOSFET driver (the BJT npn with pullup)
+int clk = 8;      //Pin 1 rotary encoder
+int data = 9;     //Pin 2 rotary encoder
+
+
+//Değişkenler 
+float set_temperature = 0; //encoderla setlenecek değer 
+
+float temperature_read = 0.0; //anlık değer 
+float PID_error = 0; 
+float previous_error = 0;
+float elapsedTime, Time, timePrev;
+float PID_value = 0;
+int button_pressed = 0;
+int menu_activated=0;
+float last_set_temperature = 0;
+
+int clk_State;
+int Last_State;  
+bool dt_State;  
+
+//PID değerleri 
+//////////////////////////////////////////////////////////
+int kp = 90;   int ki = 30;   int kd = 80;
+//////////////////////////////////////////////////////////
+
+int PID_p = 0;    int PID_i = 0;    int PID_d = 0;
+float last_kp = 0;
+float last_ki = 0;
+float last_kd = 0;
+
+int PID_values_fixed =0;
+
+//MAX6675
+#define MAX6675_CS   10
+#define MAX6675_SO   12
+#define MAX6675_SCK  13
+
 void setup() {
-// float a = thermocouple.readCelsius();
-// Serial.print(a);
-#ifdef __DEBUG__
-  Serial.begin(9600);
-#endif
-  pinMode(mosfet_pin, OUTPUT); // MOSFET output PIN
-  pinMode(CLK_PIN, INPUT); // Encoer Clock Pin
-  pinMode(DATA_PIN, INPUT); //Encoder Data Pin
-  pinMode(SW_PIN, INPUT_PULLUP);// Encoder SW Pin
-  pid.begin();          // initialize the PID instance
-  pid.setpoint(150);    // The "goal" the PID controller tries to "reach"
-  pid.tune(__Kp, __Ki,__Kd);    // Tune the PID, arguments: kP, kI, kD
-  pid.limit(0, 255);    // Limit the PID output between 0 and 255, this is important to get rid of integral windup!
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-#ifdef __DEBUG__
-    Serial.println(F("SSD1306 allocation failed"));
-#endif
-    for (;;); // Don't proceed, loop forever
-  }
-  //
-  display.setRotation(2); //Rotate the Display
-  display.display(); //Show initial display buffer contents on the screen -- the library initializes this with an Adafruit splash screen.
-  display.clearDisplay(); // Cleear the Display
-  display.setTextSize(2); // Set text Size
-  display.setTextColor(WHITE); // set LCD Colour
-  display.setCursor(48, 0); // Set Cursor Position
-  display.println("PID"); // Print the this Text
-  display.setCursor(0, 20);  // Set Cursor Position
-  display.println("Temperatur"); // Print the this Text
-  display.setCursor(22, 40); // Set Cursor Position
-  display.println("Control"); // Print the this Text
-  display.display(); // Update the Display
-  delay(2000); // Delay of 200 ms
-}
-
-
-void loop()
-{
-  float a = thermocouple.readCelsius();
-  Serial.print(a);
-  read_encoder(); //Call The Read Encoder Function
-  set_temp(); // Call the Set Temperature Function
-  if (encoder_btn_count == 1) // check if the button is pressed and its in Free Running Mode -- in this mode the arduino continiously updates the screen and adjusts the PWM output according to the temperature.
-  {
-    temperature_value_c = thermocouple.readCelsius(); // Read the Temperature using the readCelsius methode from MAX6675 Library.
-    Serial.print(temperature_value_c);
-    int output = pid.compute(temperature_value_c);    // Let the PID compute the value, returns the optimal output
-    analogWrite(mosfet_pin, output);           // Write the output to the output pin
-    pid.setpoint(set_temperature); // Use the setpoint methode of the PID library to
-    display.clearDisplay(); // Clear the display
-    display.setTextSize(2); // Set text Size
-    display.setCursor(16, 0); // Set the Display Cursor
-    display.print("Cur Temp."); //Print to the Display
-    display.setCursor(45, 25);// Set the Display Cursor
-    display.print(temperature_value_c); // Print the Temperature value to the display in celcius
-    Serial.print(temperature_value_c);
-    display.display(); // Update the Display
-#ifdef __DEBUG__
-    Serial.print(temperature_value_c); // Print the Temperature value in *C on serial monitor
-    Serial.print("   "); // Print an Empty Space
-    Serial.println(output); // Print the Calculate Output value in the serial monitor.
-#endif
-    delay(200); // Wait 200ms to update the OLED dispaly.
-  }
-}
-
-
-void set_temp()
-{
-  if (encoder_btn_count == 2) // check if the button is pressed twice and its in temperature set mode.
-  {
-    display.clearDisplay(); // clear the display
-    display.setTextSize(2); // Set text Size
-    display.setCursor(16, 0); // set the diplay cursor
-    display.print("Set Temp."); // Print Set Temp. on the display
-    display.setCursor(45, 25); // set the cursor
-    display.print(set_temperature);// print the set temperature value on the display
-    Serial.print(set_temperature);
-    display.display(); // Update the Display
-  }
-}
-
-
-
-void read_encoder() // In this function we read the encoder data and increment the counter if its rotaing clockwise and decrement the counter if its rotating counter clockwis
-{
-  clockPin = digitalRead(CLK_PIN); // we read the clock pin of the rotary encoder
-  if (clockPin != clockPinState  && clockPin == 1) { // if this condition is true then the encoder is rotaing counter clockwise and we decremetn the counter
-    if (digitalRead(DATA_PIN) != clockPin) set_temperature = set_temperature - 3;  // decrmetn the counter.
-    else  set_temperature = set_temperature + 3; // Encoder is rotating CW so increment
-    if (set_temperature < 1 )set_temperature = 1; // if the counter value is less than 1 the set it back to 1
-    if (set_temperature > 150 ) set_temperature = 150; //if the counter value is grater than 150 then set it back to 150 
-#ifdef __DEBUG__
-    Serial.println(set_temperature); // print the set temperature value on the serial monitor window
-#endif
-  }
-  clockPinState = clockPin; // Remember last CLK_PIN state
+  pinMode(PWM_pin,OUTPUT);
+  TCCR2B = TCCR2B & B11111000 | 0x03;    
+  Time = millis();
   
-  if ( digitalRead(SW_PIN) == LOW)   //If we detect LOW signal, button is pressed
-  {
-    if ( millis() - debounce > 80) { //debounce delay
-      encoder_btn_count++; // Increment the values 
-      if (encoder_btn_count > 2) encoder_btn_count = 1;
-#ifdef __DEBUG__
-      Serial.println(encoder_btn_count);
-#endif
-    }
-    debounce = millis(); // update the time variable
-  }
+  Last_State = (PINB & B00000001);      
+
+  PCICR |= (1 << PCIE0);                                       
+  PCMSK0 |= (1 << PCINT0);  
+  PCMSK0 |= (1 << PCINT1);  
+  PCMSK0 |= (1 << PCINT3);  //Set pin D11 trigger an interrupt on state change.   
+                           
+  pinMode(11,INPUT);
+  pinMode(9,INPUT);
+  pinMode(8,INPUT);
+
+  lcd.init();
+  lcd.backlight();
 }
+
+
+
+//SPI MAX6675
+double readThermocouple() {
+
+  uint16_t v;
+  pinMode(MAX6675_CS, OUTPUT);
+  pinMode(MAX6675_SO, INPUT);
+  pinMode(MAX6675_SCK, OUTPUT);
+  
+  digitalWrite(MAX6675_CS, LOW);
+  delay(1);
+
+ 
+  
+  v = shiftIn(MAX6675_SO, MAX6675_SCK, MSBFIRST);
+  v <<= 8;
+  v |= shiftIn(MAX6675_SO, MAX6675_SCK, MSBFIRST);
+  
+  digitalWrite(MAX6675_CS, HIGH);
+  if (v & 0x4) 
+  {    
+    
+    return NAN;     
+  }
+
+ 
+  v >>= 3;
+
+  
+  return v*0.25;
+}
+
+
+
+
+
+
+
+
+
+
+//interruption vector 
+ISR(PCINT0_vect){
+if(menu_activated==1)
+   {
+  clk_State =   (PINB & B00000001); 
+  dt_State  =   (PINB & B00000010); 
+  if (clk_State != Last_State){     
+     
+     if (dt_State != clk_State) { 
+       set_temperature = set_temperature+0.5 ;
+     }
+     else {
+       set_temperature = set_temperature-0.5;
+     } 
+  }
+  Last_State = clk_State;
+} 
+
+if(menu_activated==2)
+   {
+  clk_State =   (PINB & B00000001); 
+  dt_State  =   (PINB & B00000010); 
+  if (clk_State != Last_State){     
+   
+     if (dt_State != clk_State) { 
+       kp = kp+1 ;
+     }
+     else {
+       kp = kp-1;
+     } 
+  }
+  Last_State = clk_State;
+} 
+
+
+if(menu_activated==3)
+   {
+  clk_State =   (PINB & B00000001); 
+  dt_State  =   (PINB & B00000010); 
+  if (clk_State != Last_State){     
+     
+     if (dt_State != clk_State) { 
+       ki = ki+1 ;
+     }
+     else {
+       ki = ki-1;
+     } 
+  }
+  Last_State = clk_State;
+}
+
+ if(menu_activated==4)
+   {
+  clk_State =   (PINB & B00000001); 
+  dt_State  =   (PINB & B00000010); 
+  if (clk_State != Last_State){     
+     
+     if (dt_State != clk_State) { 
+       kd = kd+1 ;
+     }
+     else {
+       kd = kd-1;
+     } 
+  }
+  Last_State = clk_State;
+}
+   
+
+
+
+  if (PINB & B00001000) 
+  {       
+    button_pressed = 1;
+  } 
+  //navigate through the 4 menus with each button pressed
+  else if(button_pressed == 1)
+  {
+   
+   if(menu_activated==4)
+   {
+    menu_activated = 0;  
+    PID_values_fixed=1;
+    button_pressed=0; 
+    delay(1000);
+   }
+
+   if(menu_activated==3)
+   {
+    menu_activated = menu_activated + 1;  
+    button_pressed=0; 
+    kd = kd + 1; 
+    delay(1000);
+   }
+
+   if(menu_activated==2)
+   {
+    menu_activated = menu_activated + 1;  
+    button_pressed=0; 
+    ki = ki + 1; 
+    delay(1000);
+   }
+
+   if(menu_activated==1)
+   {
+    menu_activated = menu_activated + 1;  
+    button_pressed=0; 
+    kp = kp + 1; 
+    delay(1000);
+   }
+
+
+   if(menu_activated==0 && PID_values_fixed != 1)
+   {
+    menu_activated = menu_activated + 1;  
+    button_pressed=0;
+    set_temperature = set_temperature+1;   
+    delay(1000);
+   }
+   PID_values_fixed = 0;
+   
+  }  
+}
+
+void loop() {
+
+if(menu_activated==0)
+{
+  //eal value of temperature
+  temperature_read = readThermocouple();
+  //error between the setpoint and the real value
+  PID_error = set_temperature - temperature_read + 3;
+  //P value
+  PID_p = 0.01*kp * PID_error;
+  // I value in a range on +-3
+  PID_i = 0.01*PID_i + (ki * PID_error);
+  
+
+  //derivative zamanla temp artış oranı 
+  timePrev = Time;                            
+  Time = millis();                            
+  elapsedTime = (Time - timePrev) / 1000; 
+  //D value
+  PID_d = 0.01*kd*((PID_error - previous_error)/elapsedTime);
+  // P + I + D
+  PID_value = PID_p + PID_i + PID_d;
+
+  //PWM  0  255
+  if(PID_value < 0)
+  {    PID_value = 0;    }
+  if(PID_value > 255)  
+  {    PID_value = 255;  }
+  
+  
+  analogWrite(PWM_pin,255-PID_value);
+  previous_error = PID_error;    
+
+  delay(250); 
+  //lcd.clear();
+  
+  lcd.setCursor(0,0);
+  lcd.print("PID TEMP control");
+  lcd.setCursor(0,1);
+  lcd.print("S:");
+  lcd.setCursor(2,1);
+  lcd.print(set_temperature,1);
+  lcd.setCursor(9,1);
+  lcd.print("R:");
+  lcd.setCursor(11,1);
+  lcd.print(temperature_read,1);
+}//PID control
+
+
+
+
+//temp setpoint
+if(menu_activated == 1)
+{
+   analogWrite(PWM_pin,255);
+  if(set_temperature != last_set_temperature)
+  {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Set  temperature");    
+  lcd.setCursor(0,1);
+  lcd.print(set_temperature);  
+  }
+  last_set_temperature = set_temperature;
+  
+ 
+}
+
+
+
+
+
+//P set
+if(menu_activated == 2)
+{
+  
+  if(kp != last_kp)
+  {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Set   P  value  ");    
+  lcd.setCursor(0,1);
+  lcd.print(kp);  
+  }
+  last_kp = kp;
+  
+ 
+}
+
+
+//I set
+if(menu_activated == 3)
+{
+  
+  if(ki != last_ki)
+  {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Set   I  value  ");    
+  lcd.setCursor(0,1);
+  lcd.print(ki);  
+  }
+  last_ki = ki;
+  
+ 
+}
+
+
+
+
+//D set
+if(menu_activated == 4)
+{
+  
+  if(kd != last_kd)
+  {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Set   D  value  ");    
+  lcd.setCursor(0,1);
+  lcd.print(kd);  
+  }
+  last_kd = kd;
+}
+  
+}//Loop end
